924141d2a93d85a71ff3070baef305fd0755e5cc
Luna <imlvnaa@gmail.com>
2023-10-10 16:05:53 -0400

    Global state

--- genesis/app/src/commonMain/kotlin/xyz/genesisapp/genesis/app/App.kt
+++ genesis/app/src/commonMain/kotlin/xyz/genesisapp/genesis/app/App.kt
@@ -1,14 +1,36 @@
 package xyz.genesisapp.genesis.app
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.*
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import xyz.genesisapp.genesis.app.theme.ThemeProvider
+import xyz.genesisapp.genesis.app.theme.builtin.Catppuccin
 import xyz.genesisapp.genesis.app.views.LoginScreen
+
+class GlobalState {
+    var currentTheme by mutableStateOf(Catppuccin.Mocha.Rosewater)
+}
+
+val LocalAppState = compositionLocalOf { GlobalState() }
+
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    LoginScreen()
+    CompositionLocalProvider(LocalAppState provides GlobalState()) {
+        ThemeProvider(LocalAppState.current.currentTheme) {
+            Scaffold(
+                modifier = androidx.compose.ui.Modifier
+                    .background(LocalAppState.current.currentTheme.getColors().background)
+                    .fillMaxSize()
+            ) { pv ->
+
+                LoginScreen(LocalAppState.current, pv)
+            }
+        }
+    }
 }
 
 expect fun getPlatformName(): String
--- genesis/app/src/commonMain/kotlin/xyz/genesisapp/genesis/app/views/Login.kt
+++ genesis/app/src/commonMain/kotlin/xyz/genesisapp/genesis/app/views/Login.kt
@@ -1,10 +1,8 @@
 package xyz.genesisapp.genesis.app.views
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
@@ -14,127 +12,119 @@ import androidx.compose.ui.draw.paint
 import androidx.compose.ui.text.input.KeyboardType
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
-import xyz.genesisapp.genesis.app.theme.ThemeProvider
+import xyz.genesisapp.genesis.app.GlobalState
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun LoginScreen() {
+fun LoginScreen(globalState: GlobalState, pv: PaddingValues) {
     var isLogin by remember { mutableStateOf(false) }
     var isSplash by remember { mutableStateOf(true) }
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    ThemeProvider {
-        val colorscheme = this
-        Scaffold(
+
+    if (isSplash) {
+        Box(
             modifier = Modifier
-                .background(background)
+                .padding(pv)
                 .fillMaxSize()
-        ) { pv ->
-            if (isSplash) {
-                Box(
-                    modifier = Modifier
-                        .padding(pv)
-                        .fillMaxSize()
-                        .paint(
-                            painterResource("images/img_landing_splash.png")
-                        )
+                .paint(
+                    painterResource("images/img_landing_splash.png")
+                )
 
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = "Welcome to Genesis",
-                            color = colorscheme.primary,
-                        )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Welcome to Genesis",
+                    color = globalState.currentTheme.getColors().primary,
+                )
 
-                        Button(
-                            onClick = {
-                                isLogin = true
-                                isSplash = false
-                                password = ""
-                            }
-                        ) {
-                            Text(
-                                text = "Login",
-                                color = colorscheme.primary,
-                            )
-                        }
-
-                        Button(
-                            onClick = {
-                                isLogin = false
-                                isSplash = false
-                                password = ""
-                            }
-
-                        ) {
-                            Text(
-                                text = "Sign Up",
-                                color = colorscheme.primary,
-                            )
-                        }
+                Button(
+                    onClick = {
+                        isLogin = true
+                        isSplash = false
+                        password = ""
                     }
-
+                ) {
+                    Text(
+                        text = "Login",
+                        color = globalState.currentTheme.getColors().primary,
+                    )
                 }
-            } else {
-                Box(
-                    modifier = Modifier
-                        .padding(pv)
-                        .fillMaxSize()
 
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            if (isLogin) "Login" else "Sign Up",
-                            color = colorscheme.primary,
-                        )
-
-                        TextField(
-                            value = username,
-                            onValueChange = { username = it },
-                            label = {
-                                Text(
-                                    text = "Username",
-                                    color = colorscheme.primary,
-                                )
-                            }
-                        )
-
-                        TextField(
-                            value = password,
-                            onValueChange = { password = it },
-                            label = {
-                                Text(
-                                    text = "Password",
-                                    color = colorscheme.primary,
-                                )
-                            },
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Password
-                            )
-                        )
-
-                        Button(
-                            onClick = {}
-                        ) {
-                            Text(
-                                if (isLogin) "Login" else "Sign Up",
-                                color = colorscheme.primary,
-                            )
-                        }
+                Button(
+                    onClick = {
+                        isLogin = false
+                        isSplash = false
+                        password = ""
                     }
 
+                ) {
+                    Text(
+                        text = "Sign Up",
+                        color = globalState.currentTheme.getColors().primary,
+                    )
                 }
             }
+
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .padding(pv)
+                .fillMaxSize()
+
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    if (isLogin) "Login" else "Sign Up",
+                    color = globalState.currentTheme.getColors().primary,
+                )
+
+                TextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = {
+                        Text(
+                            text = "Username",
+                            color = globalState.currentTheme.getColors().primary,
+                        )
+                    }
+                )
+
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = {
+                        Text(
+                            text = "Password",
+                            color = globalState.currentTheme.getColors().primary,
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    )
+                )
+
+                Button(
+                    onClick = {}
+                ) {
+                    Text(
+                        if (isLogin) "Login" else "Sign Up",
+                        color = globalState.currentTheme.getColors().primary,
+                    )
+                }
+            }
+
         }
     }
 }
